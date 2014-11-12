9239421e1a6b25953f48a1004acb3520b7f73ee3
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-11-12 16:49:53 -0500

    Fix instantiation of electrode.SC_Electrode
    The PhysicalProperty attributes cannot be set with
    `astropy.constants`, so I'm just stripping the values out of the
    respective `astropy.constants`.

--- bin/fig50.py
+++ bin/fig50.py
@@ -72,18 +72,18 @@ class f50(datac.Datac):
         return fig
 
 input_params = {
-    "temp": 300,
+    "temp": 300.,
     "barrier": 2.4,
     "richardson": 100,
 
     # Parameters for Si
     # Schwede et.al. \cite{10.1038/nmat2814}
-    "el_effective_mass": constants.m_e,
-    "ho_effective_mass": 0.57 * constants.m_e, 
-    "accept_conc": units.Quantity(1e19, "1/cm3"),
+    "el_effective_mass": constants.m_e.value,
+    "ho_effective_mass": 0.57 * constants.m_e.value, 
+    "accept_conc": units.Quantity(1e19,"1/cm3"),
     # Sze & Ng \cite{9780471143239}
-    "accept_ionization_energy": units.Quantity(0.044, "eV"), 
-    "bandgap": 1.12, 
+    "accept_ionization_energy": units.Quantity(0.044, "eV"),
+    "bandgap": units.Quantity(1.12,"eV"),
 
     "beta_flux": 1e10,
     "beta_energy": 20000,
@@ -92,5 +92,5 @@ input_params = {
 
 abscissae = np.linspace(300,700, 50) * units.K
 
-data = f50(input_params, abscissae, "temp", electrode.Electrode.calc_richardson_current_density)
+data = f50(input_params, abscissae, "temp", electrode.SC_Electrode.calc_richardson_current_density)
 data.show()
