9d4bc43dd0459a2605a73a58e191416548b29cf6
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-11-12 13:33:03 -0500

    Adjust band diagrams to appropriate size

--- bin/params.py
+++ bin/params.py
@@ -3,5 +3,12 @@
 Common parameters for various plots.
 """
 
-band_diagram = {"fig_height": 3, 
-                "fig_width": 3,}
+from astropy import units
+
+units.imperial.enable()
+
+fig_height = units.Quantity(97.015, "mm")
+fig_width = units.Quantity(97.015, "mm")
+
+band_diagram = {"fig_height": fig_height.to("inch").value, 
+                "fig_width": fig_width.to("inch").value,}
