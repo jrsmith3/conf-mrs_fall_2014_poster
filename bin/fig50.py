8ce67a93c1a847060f19809bbd1c78b757c7c2fa
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-11-12 16:35:08 -0500

    Rewrite to use datac
    The fig50.py script now only plots data from the standard
    thermoelectron electrode. I will include bete data when available.

--- bin/fig50.py
+++ bin/fig50.py
@@ -23,77 +23,74 @@
 
 
 """
-Calculates and plots the output current density vs. temperature for the parameters listed in the current commit (21f8be36) of the paper.
+Calculates and plots both the beta-enhanced thermoelectron emission current density and the standard thermoelectron emission current density from an electrode held at an elevated temperature.
 """
 
+import os
+import inspect
 import numpy as np
 import matplotlib.pyplot as plt
-from astropy.units import Quantity
-import tec
+from astropy import units, constants
 import bete
+import electrode
+import datac
+from banddiagram import target_fig_fqfn
+
+# Location of the target directory relative to the directory in which this script is located.
+target_rel_dir = "../build"
+target_filename_ext = "eps"
+script_fqfn = os.path.realpath(__file__)
+target_fqfn = target_fig_fqfn(script_fqfn, target_rel_dir, target_filename_ext)
+
+# input_params = {
+#     "temp": 300,
+#     "barrier": 2.4,
+#     "richardson": 100,
+#     "beta_flux": 1e10,
+#     "beta_energy": 2000, #should be 20k, but I'm adjusting for 10.1063/1.1656484
+#     "beta_stop_dist": 2000,
+#     "doping_conc": 1e17,
+#     "surf_recomb_vel": 3e4,
+#     "elec_lifetime": 0.1,
+#     "elec_diff_len": 0.2,
+
+#     "position": 0,
+#     "voltage": 0,
+#     "emissivity": 0.5
+#     }
+
+class f50(datac.Datac):
+    def plot(self, **kwargs):
+        """
+        Plot figure
+        """
+        fig = plt.figure(**kwargs)
+        plt.semilogy(self.abscissae, self.ordinates.to("A/cm2"))
+
+        plt.ylabel("Output current density [$Acm^{-2}$]")
+        plt.xlabel("Temperature [K]")
+        return fig
 
 input_params = {
     "temp": 300,
     "barrier": 2.4,
     "richardson": 100,
+
+    # Parameters for Si
+    # Schwede et.al. \cite{10.1038/nmat2814}
+    "el_effective_mass": constants.m_e,
+    "ho_effective_mass": 0.57 * constants.m_e, 
+    "accept_conc": units.Quantity(1e19, "1/cm3"),
+    # Sze & Ng \cite{9780471143239}
+    "accept_ionization_energy": units.Quantity(0.044, "eV"), 
+    "bandgap": 1.12, 
+
     "beta_flux": 1e10,
-    "beta_energy": 2000, #should be 20k, but I'm adjusting for 10.1063/1.1656484
-    "beta_stop_dist": 2000,
-    "doping_conc": 1e17,
-    "surf_recomb_vel": 3e4,
-    "elec_lifetime": 0.1,
-    "elec_diff_len": 0.2,
+    "beta_energy": 20000,
+    "ehp_efficiency": 0.1,
+}
 
-    "position": 0,
-    "voltage": 0,
-    "emissivity": 0.5
-    }
+abscissae = np.linspace(300,700, 50) * units.K
 
-FIGNAME = "fig50.eps"
-
-# Set up the electrode materials. I assume a Cs coating and parameters according to Wu & Kahn (10.1063/1.371191)
-el_b = bete.Electrode(input_params)
-# Adjust the barrier parameter because it is actually referring to electron affinity.
-el_b["barrier"] = 0.5
-
-# Set up the control material.
-el_control = tec.Electrode(input_params)
-
-
-temps = Quantity(np.linspace(300,700,100),"K")
-output_current_densities = Quantity([], "A/m2")
-control_output_current_densities = Quantity([],"A/m2")
-
-for temp in temps:
-    # pea bete electrode
-    el_b["temp"] = temp
-    output_current_density = \
-        el_b.calc_beta_enhanced_saturation_current_density()
-
-    appended_array = np.append(output_current_densities,
-        output_current_density)
-
-    output_current_densities = Quantity(appended_array,
-        output_current_densities.unit)
-
-    # "regular" electrode
-    el_control["temp"] = temp
-    control_output_current_density = \
-        el_control.calc_saturation_current_density()
-
-    appended_array = np.append(control_output_current_densities,
-        control_output_current_density)
-
-    control_output_current_densities = Quantity(appended_array,
-        control_output_current_densities.unit)
-
-y_unit = "A/cm2"
-
-plt.semilogy(temps,output_current_densities.to(y_unit), "b")
-plt.semilogy(temps,control_output_current_densities.to(y_unit), "r")
-
-plt.legend(["bete","control"],loc="lower right")
-plt.xlabel("Temperature " + str(temps.unit))
-plt.ylabel("Current Density " + y_unit)
-plt.savefig(FIGNAME, bbox_inches=0)
-# plt.show()
+data = f50(input_params, abscissae, "temp", electrode.Electrode.calc_richardson_current_density)
+data.show()
