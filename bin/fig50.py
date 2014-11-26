5f9a8f46f738f40727bfc3cbd516979c6bd1aab9
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-11-26 16:09:44 -0500

    Upgrade to bete@2014-11-25_conf-mrs_fall_2014
    I updated the `tec` module to the `bete` branch, tag 2014-11-25_conf-mrs_fall_2014. These changes reflect the new API.

--- bin/fig50.py
+++ bin/fig50.py
@@ -31,8 +31,7 @@ import inspect
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy import units, constants
-import bete
-import electrode
+import tec
 import datac
 from banddiagram import target_fig_fqfn
 
@@ -79,11 +78,11 @@ input_params = {
 
     # Parameters for Si
     # Schwede et.al. \cite{10.1038/nmat2814}
-    "el_effective_mass": constants.m_e.value,
-    "ho_effective_mass": 0.57 * constants.m_e.value, 
-    "accept_conc": units.Quantity(1e19,"1/cm3"),
+    "electron_effective_mass": constants.m_e.value,
+    "hole_effective_mass": 0.57 * constants.m_e.value, 
+    "acceptor_concentration": units.Quantity(1e19,"1/cm3"),
     # Sze & Ng \cite{9780471143239}
-    "accept_ionization_energy": units.Quantity(0.044, "eV"),
+    "acceptor_ionization_energy": units.Quantity(0.044, "eV"),
     "bandgap": units.Quantity(1.12,"eV"),
 
     "beta_flux": 1e10,
@@ -91,10 +90,10 @@ input_params = {
     "ehp_efficiency": 0.1,
 }
 
-abscissae = np.linspace(300,700, 50) * units.K
+abscissae = np.linspace(300, 1000, 10) * units.K
 
-bete_data = f50(input_params, abscissae, "temp", bete.Bete_Electrode.calc_bete_current_density)
-conv_data = f50(input_params, abscissae, "temp", electrode.SC_Electrode.calc_richardson_current_density)
+bete_data = f50(input_params, abscissae, "temp", tec.electrode.BETE.calc_thermoelectron_current_density)
+conv_data = f50(input_params, abscissae, "temp", tec.electrode.SC.calc_thermoelectron_current_density)
 
 fig = bete_data.plot()
 conv_data.plot(fig = fig)
