3f90903b8eaf1ee5985ba584beb7c65df5f1bfd9
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-11-26 16:21:05 -0500

    Remove cruft

--- bin/fig50.py
+++ bin/fig50.py
@@ -41,23 +41,6 @@ target_filename_ext = "eps"
 script_fqfn = os.path.realpath(__file__)
 target_fqfn = target_fig_fqfn(script_fqfn, target_rel_dir, target_filename_ext)
 
-# input_params = {
-#     "temp": 300,
-#     "barrier": 2.4,
-#     "richardson": 100,
-#     "beta_flux": 1e10,
-#     "beta_energy": 2000, #should be 20k, but I'm adjusting for 10.1063/1.1656484
-#     "beta_stop_dist": 2000,
-#     "doping_conc": 1e17,
-#     "surf_recomb_vel": 3e4,
-#     "elec_lifetime": 0.1,
-#     "elec_diff_len": 0.2,
-
-#     "position": 0,
-#     "voltage": 0,
-#     "emissivity": 0.5
-#     }
-
 class f50(datac.Datac):
     def plot(self, fig = None, **kwargs):
         """
