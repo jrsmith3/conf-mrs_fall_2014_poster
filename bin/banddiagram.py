34f640a3347e0123f519f58402d6cf40e5b9505c
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-12-02 15:17:00 -0500

    Depict barrier not a function of temperature
    I had to add some band diagrams that show that the model does not
    consider the barrier to be a function of temperature, despite the fact
    that a semiconductor tends to become more intrinsic as its temperature
    increases.

--- bin/banddiagram.py
+++ bin/banddiagram.py
@@ -99,3 +99,57 @@ class BandDiagram(object):
     def showfig(self, fig_height = 3, fig_width = 3):
         self.plotfig(fig_height, fig_width)
         plt.show()
+
+
+class Model(BandDiagram):
+    """
+    Subclass which doesn't print the Fermi distribution.
+    """
+
+    def plotfig(self, fig_height = 3, fig_width = 3):
+        energy = np.linspace(-10, 10, self.n_points)
+
+        if self.quasi_fermi_level:
+            f = fermi(energy, self.temp, self.quasi_fermi_level)
+        else:
+            f = fermi(energy, self.temp, self.fermi_level)
+
+        # Band structure and Fermi energy
+        dist_width = np.linspace(0, 1.2, 2)
+
+        valence_band = self.vbm * np.ones(2)
+        conduct_band = self.cbm * np.ones(2)
+        fermi_energy = self.fermi_level * np.ones(2)
+
+        x_hi = 0.2
+        x_lo = -1.2
+        y_lo = -2.0
+        y_hi = 10.0
+
+        fig = plt.figure(figsize=(fig_height,fig_width))
+
+        # # Fermi distribution
+        # plt.plot(-f, energy, "r", linewidth = 5.0)
+
+        # Band structure and Fermi energy
+        if not self.metal:
+            plt.plot(-dist_width, valence_band, "k",)
+            plt.plot(-dist_width, conduct_band, "k",)
+        plt.plot(-dist_width, fermi_energy, "k--",)
+        if self.quasi_fermi_level:
+            quasi_fermi_energy = self.quasi_fermi_level * np.ones(2)
+            plt.plot(-dist_width, quasi_fermi_energy, "k-.",)
+
+        # E_vac
+        plt.plot([0, -0.05], [self.evac, self.evac], "k",)
+
+        # Surface
+        plt.plot([0, 0], [y_lo, y_hi], "k",)
+
+        # Limits, axes, etc.
+        plt.ylim(y_lo, y_hi)
+        plt.xlim(x_lo, x_hi)
+
+        fig.axes[0].set_frame_on(False)
+        fig.axes[0].xaxis.set(visible = False)
+        fig.axes[0].yaxis.set(visible = False)    
---
+++ bin/fig1001.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import os
+import inspect
+import numpy as np
+import matplotlib.pyplot as plt
+from banddiagram import Model, target_fig_fqfn, fermi
+from params import band_diagram
+
+# Location of the target directory relative to the directory in which this script is located.
+target_rel_dir = "../build"
+target_filename_ext = "eps"
+script_fqfn = os.path.realpath(__file__)
+target_fqfn = target_fig_fqfn(script_fqfn, target_rel_dir, target_filename_ext)
+
+fermi_level = 2.
+evac = fermi_level + 5.
+
+bd = Model(temp = 1., 
+           fermi_level = fermi_level,
+           evac = evac,)
+bd.savefig(target_fqfn, band_diagram["fig_height"], band_diagram["fig_width"])
